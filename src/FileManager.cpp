779a44b8d8ae107a38ec2885df4deab871692326
datross <datross@openmailbox.org>
2017-01-17 09:46:32 +0100

    put message of file loading

--- src/FileManager.cpp
+++ src/FileManager.cpp
@@ -1,5 +1,6 @@
 #include "FileManager.h"
 #include <fstream>
+#include <iostream>
 
 using namespace idl;
 using namespace std;
@@ -19,10 +20,11 @@ void FileManager::init(string path) {
 }
 
 json FileManager::loadJSONFile(string path) {
+	cout << "Loading file : " << getPath(path) << endl;
 	ifstream file(getPath(path));
 	json j;
 	if(!file) {
-		cerr << "Cannot open file : " << path << endl;
+		cerr << "Cannot open file : " << getPath(path) << endl;
 		return j;
 	}
 	j << file;
@@ -31,6 +33,7 @@ json FileManager::loadJSONFile(string path) {
 
 ofxSVG FileManager::loadSVGFile(string path) {
 	ofxSVG svg;
+	cout << "Loading file : " << dirPath + "/" + path << endl;
 	svg.load(dirPath + "/" + path);
 	return svg;
 }
