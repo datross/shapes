8d4a1d03ba49cd07565348358e6c47dbc549556a
JulieCapucine <jujucapucine.jp@gmail.com>
2017-01-24 09:33:56 +0100

    viewport added

--- obj/linux64/Debug/src/modifier.d
+++
@@ -1,5 +0,0 @@
-obj/linux64/Debug/src/modifier.o: \
- /home/julie/Desktop/OF/apps/myApps/shapes_try/src/modifier.cpp \
- /home/julie/Desktop/OF/apps/myApps/shapes_try/headers/Modifier.h
-
-/home/julie/Desktop/OF/apps/myApps/shapes_try/headers/Modifier.h:
--- obj/linux64/Release/src/modifier.d
+++
@@ -1,5 +0,0 @@
-obj/linux64/Release/src/modifier.o: \
- /home/julie/Desktop/OF/apps/myApps/shapes_try/src/modifier.cpp \
- /home/julie/Desktop/OF/apps/myApps/shapes_try/headers/Modifier.h
-
-/home/julie/Desktop/OF/apps/myApps/shapes_try/headers/Modifier.h:
